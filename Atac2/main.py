598f7f9fd93bcb3dc390cecbef4e9df57f82d954
Mihai Ignat <mihaiadrian009@gmail.com>
2021-11-18 14:39:37 +0200

    adde head or tail gamble for atack

--- Atac2/main.py
+++ Atac2/main.py
@@ -1,15 +1,31 @@
-lista=[["warrior",100,10,10], ["mage",100,8,15], ["rogue",100,12,8], ["paladin",100,5,20]]
+lista=[["Warrior",100,10,10], ["Mage",100,8,15], ["Rogue",100,12,8], ["Paladin",100,5,20]]
 print("Players Avaible (Name/Hp/Atk/Stamina): ",lista)
+#stamina will count as chance of critical hits
 first=int(input("Chose the player from 0 to 3: "))
 second=int(input("Chose the player from 0 to 3: "))
-player1=lista[first]
-player2=lista[second]
-print("First Fight between player:",player1[0],"and player:",player2[0])
-#player from listB starts first
-while player1[1]>0 and player2[1]>0:
-    player1[1]-=player2[2]
-    player2[1]-=player1[2]
-if player1[1]>player2[1]:
-    print("Winner is:",player1[0])
+player1=lista[first] #remeber the statistics for player1
+player2=lista[second] #and player2
+print("Fight between player:",player1[0],"and player:",player2[0])
+#who starts first ?
+#a flip the coin ideea (if odd number starts first(head), if even starts second(tails))
+HoT=int(input("Head or Tails? Chose a number from 1 and 2"))
+if HoT%2==1:
+#fight for the first player
+    print(player1[0],"Starts first")
+    while player1[1]>0 and player2[1]>0:
+        player2[1] -= player1[2]
+        player1[1] -= player2[2]
+    if player1[1] > player2[1]:
+        print("Winner is:", player1[0])
+    else:
+        print("Winner is:", player2[0])
 else:
-    print("Winner is:",player2[0])
+#fight for the second player
+    print(player2[0],"Starts first")
+    while player1[1] > 0 and player2[1] > 0:
+        player1[1] -= player2[2]
+        player2[1] -= player1[2]
+    if player1[1] > player2[1]:
+        print("Winner is:", player1[0])
+    else:
+        print("Winner is:", player2[0])
